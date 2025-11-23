b888f8fa281f8f338055aa4027f0a4558ab07af9
soadmized <soadmized1@gmail.com>
2025-11-23 18:00:33 +0300

    handle index out of range in args

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/soadmized/idconv/cmd"
+	"log"
 	"os"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("some input is required")
+	}
+
 	input := os.Args[1]
 
 	if err := cmd.Convert(input); err != nil {
